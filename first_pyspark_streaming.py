29ed4be3d1bc0bd35deccc4fb1d9c55ed63cb640
Atif Imam <mohd.atifimam@hotmail.com>
2024-01-07 17:28:02 +0530

    added first sample pyspark streaming to console

---
+++ first_pyspark_streaming.py
@@ -0,0 +1,31 @@
+import os
+
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder.appName("sparkstream").getOrCreate()
+
+# Define input sources
+lines = (
+    spark.readStream.format("socket")
+    .option("host", "localhost")
+    .option("port", 9999)
+    .load()
+)
+
+# Split the lines into words and perform word count
+words = lines.select(F.explode(F.split(F.col("value"), "\\s")).alias("word"))
+counts = words.groupBy("word").count()
+
+# Configure checkpoint directory
+checkpoint_dir = os.path.join(os.getcwd(), "checkpoint")
+
+streamingQuery = (
+    counts.writeStream.format("console")
+    .outputMode("complete")  # Adjust based on your use case
+    .trigger(processingTime="1 second")
+    .option("checkpointLocation", checkpoint_dir)
+    .start()
+)
+
+streamingQuery.awaitTermination()
